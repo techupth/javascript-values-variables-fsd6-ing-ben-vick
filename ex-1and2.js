2cf2ab7a0203a39674750fa328418c644bf75a79
jarudech <jarudech013@gmail.com>
2024-05-09 12:33:40 +0700

    did exercise 1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const price = 15000;
+const restaurantNumber = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+price = 5000;
+restaurantNumber = 2500;
